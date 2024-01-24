d931f9cffd41cca3f9ec7b86d491bd4e8e5d0b35
jiaching.lin <114284930+Kei0618@users.noreply.github.com>
2024-01-24 15:48:03 +0800

    Merge pull request #1 from Kei0618/User-1
    User-1 新增SignUp方法

---
+++ Auth.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitPractice
+{
+    public class Auth
+    {
+        public void SignUp()
+        {
+            var _user = "王大明";
+        }
+    }
+}
